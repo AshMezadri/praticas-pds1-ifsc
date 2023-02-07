4a6ae9e52b5b0ed59623b5a4fb6d2641f1163105
Ash <ash.m10@aluno.ifsc.edu.br>
2023-02-07 08:50:05 -0300

    Teste 1

---
+++ teste_1/src/controle/FuncionarioDAO.java
@@ -0,0 +1,23 @@
+package controle;
+
+import java.util.ArrayList;
+
+import modelo.Funcionarios;
+
+public class FuncionarioDAO {
+
+	private static ArrayList<Funcionarios> listaFuncionarios;
+	
+	public FuncionarioDAO() {
+		listaFuncionarios = new ArrayList<>();
+	}
+	
+	public boolean inserir(Funcionarios func) {
+		listaFuncionarios.add(func);
+		return false;
+	}
+	
+	public ArrayList<Funcionarios> listar(){
+		return listaFuncionarios;
+	}
+}
---
+++ teste_1/src/visao/Janelinha_2.java
@@ -0,0 +1,41 @@
+package visao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Janelinha_2 extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Janelinha_2 frame = new Janelinha_2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Janelinha_2() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
